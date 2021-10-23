02553c6e6b9209710946f4e0d7795c6dcaaf55bf
Murshid98 <abdullamurshidkv@gmail.com>
2021-10-23 23:06:24 +0530

    Changes made in iam rich app

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('I AM RICH'),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0x403CB1FF),
+        appBar: AppBar(
+          title: const Text("I AM RICH"),
+          backgroundColor: Color(0xFF0C163B),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png')
+          ),
+        ),
       ),
     ),
   );
